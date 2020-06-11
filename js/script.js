fb45d418b7cd786a85f1ddc83009080cdc9349dc
Jackson Magnabosco <jacksonmagnabosco@hotmail.com>
2020-06-11 11:42:04 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,32 +1,52 @@
 var botaoLogar = document.querySelector("#logar");
 botaoLogar.addEventListener("click", function (event) {
-    event.preventDefault();
+  event.preventDefault();
+
+  var erros = validar();
+
+  if (erros.length > 0) {
+    console.log(exibeMensagensDeErro(erros));
+    exibeMensagensDeErro(erros);
+    return;
+  }
+
+  if (erros.length == 0) {
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
+    mensagensErro.value = "";
+  }
+});
+
+
+function exibeMensagensDeErro(erros) {
+  var ul = document.querySelector("#mensagens-erro");
+  ul.innerHTML = "";
+
+  erros.forEach(function (erro) {
+    var li = document.createElement("li");
+    li.textContent = erro;
+    ul.appendChild(li);
+  });
+}
+
+function validar() {
 
   var email = form1.email.value;
   var senha = form1.password.value;
+  var erros = [];
 
-  if (email == "") {
-    alert('Campo email Obrigatório.');
-    form1.txtemail.focus();
-    return false;
+  if (email.length == 0) {
+    erros.push('Campo email Obrigatório.');
   }
 
   if ((email != 0) && ((email.indexOf("@") < 1) || (email.indexOf('.') < 7))) {
-    alert('Informe um email Válido.');
-    form1.txtemail.focus();
-    return false;
+    erros.push('Informe um email Válido.');
   }
 
-  if (senha == "") {
-    alert('Campo senha Obrigatório.');
-    form1.txtsenha.focus();
-    return false;
+  if (senha.length == 0) {
+    erros.push('Campo senha Obrigatório.');
   }
 
-  if (senha.length < 6) {
-    alert('Informe uma senha com mais de 5 caracteres.');
-    form1.txtsenha.focus();
-    return false;
-  }
+  return erros;
+}
 
-});
